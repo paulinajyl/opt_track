82bb4227e46d7c9dcd1c8534e1d1046f1b7d32c4
JiYoung Lee <paulinalee@uni.minerva.edu>
2025-04-13 20:24:30 -0700

    go back to hard db

--- src/database.py
+++ src/database.py
@@ -3,10 +3,22 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-DATABASE_URL = os.environ['DATABASE_URL']
+
+# Fetch variables
+USER = os.getenv("user")
+PASSWORD = os.getenv("password")
+HOST = os.getenv("host")
+PORT = os.getenv("port")
+DBNAME = os.getenv("dbname")
 
 def get_connection():
-    connection = psycopg2.connect(DATABASE_URL)
+    connection = psycopg2.connect(
+        user=USER,
+        password=PASSWORD,
+        host=HOST,
+        port=PORT,
+        dbname=DBNAME
+    )
     print("Connection successful!")
     return connection
 
